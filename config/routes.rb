3a97292d2e8d2a37506fbcfadc60894c3cd569e1
GFenzo <gfenzo@miuandes.cl>
2018-05-08 14:38:59 -0300

    lab 8

---
+++ config/routes.rb
@@ -0,0 +1,23 @@
+Rails.application.routes.draw do
+  root to: 'application#index'
+
+  resources :users do
+
+    resources :products
+
+
+  end
+
+
+  get '/admin/:id', to: 'users#admin_home'
+  get '/admin/:id/users', to: 'users#admin_users'
+  get '/admin/:id/products', to: 'users#admin_products'
+  get '/admin/:id/users/:id/products', to: 'users#admin_user_product'
+  
+
+
+
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
